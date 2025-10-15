c2b549e0330ab666da85a95f47f2ab7349e6935f
Damayanti Nama <DN00862377@techmahindra.com>
2025-10-16 00:38:39 +0530

    Day 17 Templates & Static Files in Django

--- shop/products/views.py
+++ shop/products/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home page")
+    context = {
+        'title': 'Home',
+        'shop_name': 'My Shop'
+    }
+    #return HttpResponse("Home page")
+    return render(request,'home.html',context)
